1c6ffe6f8bce70371f5a705b36c4ba762be93cc4
Aistis Raulinaitis <sheganinans@gmail.com>
2023-09-09 19:41:42 -0700

    correct ordering, make param search a function

--- main.py
+++ main.py
@@ -41,25 +41,21 @@ def read_or_calc(file:str) -> np.ndarray:
     save_file({"embedding" : data}, stf)
     return data
 
-embeddings = np.array([ read_or_calc(file) for file in syms ])
-
-print("embeddings loaded")
-embeddings = normalize(embeddings, norm='l2')
-
-reducer = umap.UMAP(metric="manhattan")
-
-embeddings = reducer.fit_transform(embeddings)
-
 def filter_noise_by(descrips, data):
   return np.array([x[0] for x in zip(data, descrips) if "does not have significant operations" not in x[1]])
 
 def cleaned_data(descrips, syms, embeds):
-  descrips = filter_noise_by(descrips, descrips)
   embeds = filter_noise_by(descrips, embeds)
   syms = filter_noise_by(descrips, syms)
+  descrips = filter_noise_by(descrips, descrips)
   return descrips, syms, embeds
 
-descrips, syms, embeddings = cleaned_data(descrips, syms, embeddings)
+embeddings = np.array([ read_or_calc(file) for file in syms ])
+
+#descrips, syms, embeddings = cleaned_data(descrips, syms, embeddings)
+
+print("embeddings loaded")
+embeddings = normalize(embeddings, norm='l2')
 
 def wrap_text(r):
   acc = []
@@ -70,33 +66,35 @@ def wrap_text(r):
 
 descrips = [ wrap_text(r) for r in descrips ]
 
-logging.captureWarnings(True)
-hdb = hdbscan.HDBSCAN(gen_min_span_tree=True).fit(embeddings)
+def run_param_search():
+  logging.captureWarnings(True)
+  hdb = hdbscan.HDBSCAN(gen_min_span_tree=True).fit(embeddings)
 
-# specify parameters and distributions to sample from
-param_dist = {'min_samples': [3,5,10,15],
-              'min_cluster_size':[3,5,10,15],
-              'cluster_selection_method' : ['leaf', "eom"],
-              'cluster_selection_epsilon' : [0.02,0.0225,0.025,0.0275,0.03],
-              'metric' : ['manhattan', "euclidean"]
-             }
+  # specify parameters and distributions to sample from
+  param_dist = {'min_samples': [3,5,10,15],
+                'min_cluster_size':[3,5,10,15],
+                'cluster_selection_method' : ['leaf', "eom"],
+                'cluster_selection_epsilon' : [0.02,0.0225,0.025,0.0275,0.03],
+                'metric' : ['manhattan', "euclidean"]
+              }
 
-validity_scorer = make_scorer(hdbscan.validity.validity_index,greater_is_better=True)
+  validity_scorer = make_scorer(hdbscan.validity.validity_index,greater_is_better=True)
 
+  n_iter_search = 40
+  random_search = RandomizedSearchCV(hdb
+                                    ,param_distributions=param_dist
+                                    ,n_iter=n_iter_search
+                                    ,scoring=validity_scorer
+                                    ,random_state=0x1337)
 
-n_iter_search = 40
-random_search = RandomizedSearchCV(hdb
-                                   ,param_distributions=param_dist
-                                   ,n_iter=n_iter_search
-                                   ,scoring=validity_scorer
-                                   ,random_state=0x1337)
+  random_search.fit(embeddings)
+  print(f"Best Parameters {best}")
+  print(f"DBCV score :{random_search.best_estimator_.relative_validity_}")
 
-random_search.fit(embeddings)
+#run_param_search()
 
-best = random_search.best_params_
-print(f"Best Parameters {best}")
-print(f"DBCV score :{random_search.best_estimator_.relative_validity_}")
-
+#best = random_search.best_params_
+best = {'min_samples': 3, 'min_cluster_size': 3, 'metric': 'manhattan', 'cluster_selection_method': 'eom', 'cluster_selection_epsilon': 0.0275}
 
 clusters = hdbscan.HDBSCAN(
   min_cluster_size=best["min_cluster_size"],
@@ -104,6 +102,10 @@ clusters = hdbscan.HDBSCAN(
   cluster_selection_method=best["cluster_selection_method"],
   cluster_selection_epsilon=best["cluster_selection_epsilon"],
   metric=best["metric"]).fit_predict(embeddings)
+
+reducer = umap.UMAP(metric="manhattan")
+
+embeddings = reducer.fit_transform(embeddings)
 
 addtl_info = {"sym": syms, "cluster": clusters, "descrip": descrips}
 fig = px.scatter(
